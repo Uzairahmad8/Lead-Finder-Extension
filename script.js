a8d952f4d82272c164b76bbdbb699000c4d05e8a
Uzair Ahmad <uzairdev8@gmail.com>
2024-05-05 22:53:08 +0500

    .

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// let myLeads = ["hekki", "ahahah"];
-let inputBtn = document.getElementById("input-btn");
+let myLeads = ["hekki", "ahahah"];
+const inputBtn = document.getElementById("input-btn");
 let inputEl = document.getElementById("input-el");
-let unOrderedList = document.getElementById("unorderedList");
+let unOrderedList = document.getElementById("unOrderedList");
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
+    console.log(myLeads);
+    inputEl.value = " ";
 })
 
 // for ( let i = 0; i < myLeads.length; i++ ) {
