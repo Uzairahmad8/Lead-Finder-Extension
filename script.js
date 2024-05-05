e7f498743019e3f3e98cc4f71ce7de994dc82558
Uzair Ahmad <uzairdev8@gmail.com>
2024-05-05 23:48:20 +0500

    .

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-let myLeads = ["Uzair", "Ahmad", "Khan", "Lala", "Chacha"];
+let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 let inputEl = document.getElementById("input-el");
 let ul_list = document.getElementById("unOrderedList");
+let paragraph = document.createElement("p");
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    console.log(myLeads);
-    inputEl.value = " ";
+    if ( inputEl.value != "" && inputEl.value != " ") {
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+        paragraph.textContent = ""; 
+        renderLead();
+        
+    } else {
+        paragraph.innerHTML += "Please Entry Something <br>";
+        document.body.appendChild(paragraph);
+    }
+   
 })
 
-for ( let i = 0; i < myLeads.length; i++ ) {
-    // ul_list.innerHTML += "<li>" + myLeads[i] +  "</li>";
-    
-    // use another method instead of doing it with innerHTML.
+function renderLead() {
+    let listItems = "";
+    for ( let i = 0; i < myLeads.length; i++ ) {
+        // ul_list.innerHTML += "<li>" + myLeads[i] +  "</li>";
+        // use another method instead of doing it with innerHTML.
+        // const li = document.createElement("li");
+        // li.textContent = myLeads[i];
+        // ul_list.append(li);
+        
+        // listItems += "<li><a target = '_blank' href = '" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+            <li>
+                <a target = '_blank' href = '${myLeads[i]}'>  ${myLeads[i]}</a>
+            </li> 
+        `
+    }
 
-    const li = document.createElement("li");
-    li.textContent = myLeads[i];
-    ul_list.append(li);
+    ul_list.innerHTML = listItems;
 }
